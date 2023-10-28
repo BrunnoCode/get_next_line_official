cc69400fc2ac7db66cd2eff85d9bed482969ce96
Brunno Warley Botelho Rocha <bbotelho@cbr15s3.42barcelona.com>
2023-10-28 23:53:46 +0200

    started

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 23:34:15 by bbotelho          #+#    #+#             */
+/*   Updated: 2023/10/28 23:51:06 by bbotelho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 30
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
