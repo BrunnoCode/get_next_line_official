f362344d444472c9aa0c1735b9bd2fa32ed94b79
Brunno Warley Botelho Rocha <bbotelho@cbr15s3.42barcelona.com>
2023-10-29 02:23:13 +0200

    loading

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,24 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:51:31 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/28 23:53:11 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/29 02:22:57 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
 	
 }
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+		return NULL;
+	int	fd = open(arc[1]);
+	close(fd);
+	free(fd);
+		return (0);
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:34:15 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/28 23:51:06 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/29 01:41:50 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 30
 # endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
