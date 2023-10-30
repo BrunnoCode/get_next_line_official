b151670b41c9a3e969b80eda8634cae94bf99ed8
Brunno Warley Botelho Rocha <bbotelho@car8s4.42barcelona.com>
2023-10-30 18:30:26 +0100

    vamoss

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,35 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/28 23:51:31 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/30 18:30:06 by bbotelho         ###   ########.fr       */
+/*   Created: 2023/10/30 17:12:13 by bbotelho          #+#    #+#             */
+/*   Updated: 2023/10/30 17:13:19 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	char	*
-}
+// FUNCIONES DE AUXILIO
 
-int	main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("test.txt", O_RDONLY);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (line == 0)
-			break ;
-		printf("%s", line);
-	}
-	close(fd);
-	return (0);
-}
