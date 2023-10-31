06c6fab192000b06940ee69c9f776771900f20f6
Brunno Warley Botelho Rocha <bbotelho@car8s5.42barcelona.com>
2023-10-31 12:52:58 +0100

    doing

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:51:31 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/30 18:54:01 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/31 12:47:56 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 char	*get_next_line(int fd)
 {
-	char	*str_total;
 	char	*line;
+	char *text_readed;
+	char	*adress;
+
+	adress = &fd;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	
+		return (NULL);
+	line = read(fd, text_readed, BUFFER_SIZE);
+	if(line < 0)
+		return (NULL);
+	while()
 
 
 }
