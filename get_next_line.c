a374972cefe519275198f97f4a72f67f77c08d48
Brunno Warley Botelho Rocha <bbotelho@car5s4.42barcelona.com>
2023-11-06 13:26:35 +0100

    haciendo

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,61 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:51:31 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/31 17:44:37 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/11/06 13:26:22 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*ft_lector(int fd)
 {
-	ssize_t	*total_len;
-	static int count = 0;
-	if(count ==  5)
-		return (NULL);
-	// verifico se nao tem erro
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	// fim verificacao de erro
-	// total_len recebe o tamanho necessario de memoria para armazenar a quantidade de buffer
-	total_len = (ssize_t *)malloc(sizeof(char) * BUFFER_SIZE);
-	// fim de malloc
-	// verifico se deu certo
-	if (total_len == NULL)
-		return (NULL);
-	// fim da verificacao
-	// total_len recebe a leitura total do buffer
-	read(fd, total_len, BUFFER_SIZE);
-	count++;
-	return ("timur");
+	char	*aux;
+	int	i;
+	i = 0;
+	int lector_bytes;
+
+	lector_bytes = read(fd, lector_bytes, BUFFER_SIZE);
+	while(lector_bytes > i)
+	{
+
+	}
 }
 
+
+char	*get_next_line(int fd)
+{
+	static char	*buf;
+
+	if(BUFFER_SIZE <= 0 || fd < 0 || fd > 1024)
+		return (NULL);
+	buf = malloc(BUFFER_SIZE * sizeof(char) + 1);
+	if (!buf)
+		return (0);
+	ft_lector(fd);
+}
+
+
+
+
+
+
+
+
+
+/*
 int	main(void)
 {
-	int	fd;
-
+	int		fd;
 	char	*line;
+
 	fd = open("test.txt", O_RDONLY);
 	do
 	{
 		line = get_next_line(fd);
-		if(!line)
-			return(0);
+		if (!line)
+			return (0);
 		printf("%s", line);
-	}while(line);
+	} while (line);
 	close(fd);
 	return (0);
-}
+}*/
