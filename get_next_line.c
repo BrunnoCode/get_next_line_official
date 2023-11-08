bc4d2b938aa204590959a028ff02644e3914b9fb
Brunno Warley Botelho Rocha <bbotelho@car11s4.42barcelona.com>
2023-11-08 19:37:40 +0100

    continue

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,57 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:51:31 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/11/08 18:20:30 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/11/08 19:36:39 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	ft_join(char **line, char **sto)
+{
+	while(*sto != NULL)
+	{
+		*line = *sto;
+		line++;
+		sto++;
+	}
+}
+
+int	str_len(char *sto)
+{
+	int i;
+
+	i = 0;
+	while(*sto != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
 void	ft_lector(int fd, char **sto)
 {
 	int	bytes;
+	*sto[0] = '\0';
 
-	if (!(bytes = read(fd, *sto, BUFFER_SIZE) > 0))
+	bytes = read(fd, *sto, BUFFER_SIZE); 
+	if(bytes < 0)
 	{
-		free(*sto);
+		free(sto);
 		return ;
 	}
-	while (*sto != (char *)'\n' || *sto != (char *)'\0')
+	while (*sto != (char *)'\n' && *sto != (char *)'\0')
 	{
-		(sto++);
+		sto++;
 	}
-	sto[bytes + 1] = 0;
+	*sto[bytes] = 0;
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*storage;
+	static char	*storage = NULL;
+	char *line;
 
-	storage = NULL;
 	if (BUFFER_SIZE <= 0 || fd < 0 || fd > 1024)
 		return (NULL);
 	if (!(storage = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
@@ -40,7 +64,11 @@ char	*get_next_line(int fd)
 	ft_lector(fd, &storage);
 	if (!storage)
 		free(storage);
-	return (storage);
+	line = (char*)malloc(sizeof(char) * str_len(storage));
+		if(!line)
+			return(0);
+		ft_join(&line, &storage);
+	return (line);
 }
 
 int	main(void)
