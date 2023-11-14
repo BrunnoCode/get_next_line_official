403f406c4a2c6a64eadb2553dbe3e2630bd2db73
Brunno Warley Botelho Rocha <bbotelho@car6s6.42barcelona.com>
2023-11-14 17:20:10 +0100

    estoy aprendiendo aun

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,18 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:42:36 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/11/13 22:38:44 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/11/14 17:16:30 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	file_read(char **storage, char **line, ssize_t *n_bytes, int fd)
+void	*file_read(char **storage, char **line, int *n_bytes, int fd)
 {
 	int i;
-	*n_bytes = read(fd, *storage, BUFFER_SIZE);
+	n_bytes = (int*)read(fd, *storage, BUFFER_SIZE);
 	if(*n_bytes < BUFFER_SIZE)
-		return (0);
+		return (NULL);
 	(*line) = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if(!(*line))
 	{
@@ -28,30 +28,29 @@ int	file_read(char **storage, char **line, ssize_t *n_bytes, int fd)
 	while((*storage)[i] != '\0' || (*storage)[i] == '\n')
 	{
 		if(*storage[i] == '\n')
-			return (0);
+			return (NULL);
 		(*line)[i] = (*storage)[i];
 		i++;
 	}
 	(*line)[++i] = '\0';
 	free(storage);
-	return (0);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
 	static	char *storage = NULL;
 	char *line;
-	ssize_t	n_bytes;
+	int	n_bytes;
 	
 	if(BUFFER_SIZE <=0 || fd < 0)
 		return (NULL);
 	n_bytes = 1;
-	while(!(n_bytes <= 0))
+	while(n_bytes > 0)
 	{
 		file_read(&storage, &line, &n_bytes, fd); 
-		
 	}
-	if(n_bytes <= 0)
+	if(n_bytes <= 0 || !line)
 		return (NULL);
 	if(*storage == '\n')
 		storage++;
