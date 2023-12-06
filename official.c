f32f8a1110d98ecc85a3f6d10caa565c4400e734
Brunno Warley Botelho Rocha <bbotelho@car12s5.42barcelona.com>
2023-12-06 13:43:57 +0100

    otra vez

--- official.c
+++ official.c
@@ -6,17 +6,24 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 20:20:38 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/12/05 21:07:15 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/12/06 12:59:22 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void    ft_reading(int fd, char **storage, int *error)
+void    ft_fill_line(char **line, char **storage, int *error)
 {
-    while(error >= 0 && !check_nl())
+    
+}
+
+void    ft_reading(int fd, char **storage, char **line, int *error)
+{
+    while(error >= 0)
     {
         error = read(fd, *storage, BUFFER_SIZE);
+        if(*storage != NULL)
+             ft_fill_line(line, storage, error);
     }
 }
 
@@ -29,12 +36,10 @@ char	*get_next_line(int fd)
     error = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-    ft_reading(fd, &storage, &error);
-    if(error != -1)
-    {
-        ft_fill_line(&line)
-    }
-    free(storage);
-    free(line);
+    ft_reading(fd, &storage, &line, &error);
+    // IMPLEMENTAR VERIFICACION DE ERRORES
+    if(error < 0)
+        return (NULL);
+    //
     return (NULL);
 }
