f9bfc86d952f16610a9271d689eee75ed72816a1
bbotelho <brunnowr.bw@gmail.com>
2023-12-10 19:17:41 +0100

    vamo

---
+++ foundnl.c
@@ -0,0 +1,23 @@
+#include "get_next_line.h"
+
+int foundnl(char *buffer)
+{
+    int i;
+
+    while(buffer[i])
+    {
+        if(buffer[i] == '\n')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+/*
+#include <stdio.h>
+int main()
+{
+    char    *frase = "'\n\0'";
+    printf("%d", foundnl(frase));
+    return (0);
+
+}*/
