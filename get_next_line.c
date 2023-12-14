719194caba15303bd3fb129b5ef3bc201298ad2c
bbotelho <brunnowr.bw@gmail.com>
2023-12-14 17:25:21 +0100

    automatic save home

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 18:03:34 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/12/14 17:15:59 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:24:36 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,18 @@ char    *special_free(char **checkpoint)
 }
 int   clean_alloc(char **buffer)
 {
-    *buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    int i;
+
+    i = BUFFER_SIZE + 1;
+    *buffer = malloc(sizeof(char) * i);
     if (!*buffer)
     {
         free(*buffer);
         buffer = NULL;
         return (-1);
     }
+    while(i-- > 0)
+        *buffer[i] = '\0';
     return (1);
 }
 
